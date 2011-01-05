033bf60b35a6d10b1b48ab340908874a7033c4d9
Brian Edgerton <brian@edgewebworks.com>
2011-01-05 13:03:28 -0500

    Moved plugin dispatching to plugin itself

--- components/com_api/controllers/http.php
+++ components/com_api/controllers/http.php
@@ -12,31 +12,10 @@ class ApiControllerHttp extends ApiController {
 	
 	public function display() {
 		$this->resetDocumentType();
-		$auth	= JModel::getInstance('Authentication', 'ApiModel');
-		
-		$user	= $auth->authenticateRequest();
-		
-		if ($user === false) :
-			ApiError::raiseError(403, $auth->getError());
-		endif;
-		
 		jimport('joomla.plugin.helper');
 		$name		= JRequest::getCmd('app');
 		$handler	= ApiPlugin::getInstance($name);
-		$handler->set('user', $user);
-		$resource	= $handler->get('resource');
-		
-		if (!method_exists($handler, $resource)) :
-			ApiError::raiseError(404, JText::_('COM_API_PLUGIN_METHOD_NOT_FOUND'));
-		endif;
-		
-		if (!is_callable(array($handler, $resource))) :
-			ApiError::raiseError(404, JText::_('COM_API_PLUGIN_METHOD_NOT_CALLABLE'));
-		endif;
-		
-		$response 	= $handler->$resource();
-		$output		= $handler->encode();
-		
+		$output	= $handler->fetchResource();
 		echo $output;
 	}
 	
