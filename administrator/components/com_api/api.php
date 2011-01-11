89cbd464e85dcc14e2692877a8415d6f86acb1a9
Brian Edgerton <brian@edgewebworks.com>
2011-01-11 10:39:02 -0500

    Added support for admin controller

--- administrator/components/com_api/api.php
+++ administrator/components/com_api/api.php
@@ -6,20 +6,23 @@ jimport('joomla.application.component.controller');
 $frontside = JPATH_SITE.'/components/com_api';
 
 JLoader::register('APIController', $frontside.'/libraries/controller.php');
+JLoader::register('ApiControllerAdmin', $frontside.'/libraries/admin_controller.php');
 JLoader::register('APIModel', $frontside.'/libraries/model.php');
 JLoader::register('APIView', $frontside.'/libraries/view.php');
 
 $view	= JRequest::getCmd('view', '');
-if ($view) :
+$c		= JRequest::getCmd('c', '');
+if ($view && !$c) :
 	$c	= $view;
 endif;
 
-$c_path	= JPATH_COMPONENT.'/controllers/'.strtolower($c).'.php';
+$c_path	= JPATH_COMPONENT_ADMINISTRATOR.'/controllers/'.strtolower($c).'.php';
+
 if (file_exists($c_path)) :
 	include_once $c_path;
 	$c_name	= 'ApiController'.ucwords($c);
 else :
-	$c_name = 'ApiController';
+	$c_name = 'ApiControllerAdmin';
 endif;
 
 $command = JRequest::getCmd('task', 'display');
